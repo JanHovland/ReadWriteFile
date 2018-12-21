64d911d5cc8e48ba5ee505cb29d8ac6644780791
Jan Hovland <jan.hovland@lyse.net>
2018-12-21 12:06:52 +0100

    Initial Commit

---
+++ ReadWriteFile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadWriteFile
+//
+//  Created by Jan  on 21/12/2018.
+//  Copyright © 2018 Jan . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
