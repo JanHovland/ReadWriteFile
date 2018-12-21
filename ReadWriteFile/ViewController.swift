fcfdee26c1a66053ca7165bf280daa4a2498c939
Jan Hovland <jan.hovland@lyse.net>
2018-12-21 14:59:11 +0100

    Update

--- ReadWriteFile/ViewController.swift
+++ ReadWriteFile/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    var readString = ""
+    
+     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let filename = "Test"
+        let DocumentDirURL =  try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        let fileURL = DocumentDirURL.appendingPathComponent(filename).appendingPathExtension("txt")
+    
+        print("File Path: \(fileURL.path)")
+        
+        let writeString = "Write this text to the file in Swift"
+        do {
+            // Write to the file
+            try writeString.write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
+        } catch let error as NSError {
+            print("Failed to write to URL")
+            print(error)
+        }
+    
+        do {
+            readString = try String(contentsOf: fileURL)
+        } catch let error as NSError {
+            print("Failed to read file")
+            print(error)
+        }
+    
+        print("Contents of this file: \(readString)")
+        
     }
-
-
 }
 
